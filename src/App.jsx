1ca370309a4a0b6306dafb2e9c90aa2499e1b864
DaedraDante <153109749+DaedraDante@users.noreply.github.com>
2025-03-06 11:09:20 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+import Food from './Food.jsx'
+function App() {
+  return(
+      <>
+        <Food/>
+        <Header/>
+        <Food/>
+        <Footer/>
+        <Food/>
+      </>
+  );
+}
+
+export default App
